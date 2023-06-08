a3679e4dba4212ee4ff82e488edbb50601acd1cc
Aris Buzachis <114920449+aris-bunnyshell@users.noreply.github.com>
2023-06-08 14:29:45 +0300

    Update bunnyshell_start-stop-preview-env.yaml
    Signed-off-by: Aris Buzachis <aris.buzachis@bunnyshell.com>

--- .bunnyshell/docker/migrations/docker-entrypoint.sh
+++ .bunnyshell/docker/migrations/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+FIXTURES_INITIALIZED_FILE=/srv/sylius/public/media/.docker-initialized
+
 attempts_left=20
 
 php bin/console cache:clear
@@ -21,7 +23,7 @@ done
 
 php bin/console doctrine:migrations:migrate --no-interaction
 
-if [ "$LOAD_FIXTURES" = "1" ]; then
+if [ "$LOAD_FIXTURES" = "1" ] && [ ! -f "$FIXTURES_INITIALIZED_FILE" ]; then
     # Replace localhost with BASE_DOMAIN in fixtures.yml
     if [ -z "$BASE_DOMAIN" ]; then
         sed -i "s/localhost/$BASE_DOMAIN/g" vendor/sylius/sylius/src/Sylius/Bundle/CoreBundle/Resources/config/app/fixtures.yml
@@ -31,4 +33,6 @@ if [ "$LOAD_FIXTURES" = "1" ]; then
 
     # Generate image cache
     find public/media/image -type f -print0 | sed 's/public\/media\/image\///' | xargs -0 -I{} sh -c 'php bin/console liip:imagine:cache:resolve {} || true'
+
+    touch "$FIXTURES_INITIALIZED_FILE"
 fi
